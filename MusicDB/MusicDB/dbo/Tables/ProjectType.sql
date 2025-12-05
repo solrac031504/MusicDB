256321902596c4b6f3b2a676bb52f58e39e0a72b
solrac031504 <carlos.gonzalez031504@gmail.com>
2025-12-04 21:10:43 -0500

    Formatting projectype

--- MusicDB/MusicDB/dbo/Tables/ProjectType.sql
+++ MusicDB/MusicDB/dbo/Tables/ProjectType.sql
@@ -1,66 +1,155 @@
-﻿CREATE TABLE [dbo].[ProjectType] (
-    [ProjectTypeId]   INT            IDENTITY (1, 1) NOT NULL,
-    [ProjectTypeName] NVARCHAR (255) NOT NULL,
-    [CreatedUtc]      DATETIME       CONSTRAINT [DF_ProjectType_CreatedUtc] DEFAULT (getutcdate()) NOT NULL,
-    [CreatedBy]       NVARCHAR (255) CONSTRAINT [DF_ProjectType_CreatedBy] DEFAULT (suser_sname()) NOT NULL,
-    [ModifiedUtc]     DATETIME       NULL,
-    [ModifiedBY]      NVARCHAR (255) NULL,
-    CONSTRAINT [PK_ProjectType] PRIMARY KEY CLUSTERED ([ProjectTypeId] ASC)
+﻿CREATE TABLE [dbo].[ProjectType]
+(
+    [ProjectTypeId]           INT                 IDENTITY(1, 1)
+    , [ProjectTypeName]       NVARCHAR(255)       NOT NULL
+    , [CreatedUtc]            DATETIME            NOT NULL        CONSTRAINT [DF_ProjectType_CreatedUtc]  DEFAULT (GETUTCDATE())
+    , [CreatedBy]             NVARCHAR(255)       NOT NULL        CONSTRAINT [DF_ProjectType_CreatedBy]   DEFAULT (SUSER_SNAME())
+    , [ModifiedUtc]           DATETIME                NULL
+    , [ModifiedBy]            NVARCHAR(255)           NULL
+    , CONSTRAINT [PK_ProjectType] PRIMARY KEY CLUSTERED (ProjectTypeId ASC)
 );
-
-
 GO
-EXECUTE sp_addextendedproperty @name = N'CODEOWNER', @value = N'CARLOS', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ProjectType';
 
-
+-- ******************************
+-- EXTENDED PROPERTIES
+-- ******************************
+EXEC sys.sp_addextendedproperty
+    @name = N'Description'
+    , @value = N'Types of musical projects'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'dbo'
+    , @level1type = N'TABLE'
+    , @level1name = N'ProjectType';
 GO
-EXECUTE sp_addextendedproperty @name = N'CreatedBy', @value = N'Carlos', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ProjectType';
 
-
+EXEC sys.sp_addextendedproperty
+    @name = N'CODEOWNER'
+    , @value = N'CARLOS'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'dbo'
+    , @level1type = N'TABLE'
+    , @level1name = N'ProjectType';
 GO
-EXECUTE sp_addextendedproperty @name = N'CreatedDate', @value = N'11/22/2025', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ProjectType';
 
-
+EXEC sys.sp_addextendedproperty
+    @name = N'SQLVERSION'
+    , @value = N'SQL Azure 12.0.2000.8'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'dbo'
+    , @level1type = N'TABLE'
+    , @level1name = N'ProjectType';
 GO
-EXECUTE sp_addextendedproperty @name = N'Description', @value = N'Types of musical projects', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ProjectType';
 
-
+EXEC sys.sp_addextendedproperty
+    @name = N'Version'
+    , @value = N'1.0'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'dbo'
+    , @level1type = N'TABLE'
+    , @level1name = N'ProjectType';
 GO
-EXECUTE sp_addextendedproperty @name = N'ModifiedBy', @value = N'', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ProjectType';
 
-
+EXEC sys.sp_addextendedproperty
+    @name = N'CreatedDate'
+    , @value = N'11/22/2025'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'dbo'
+    , @level1type = N'TABLE'
+    , @level1name = N'ProjectType';
 GO
-EXECUTE sp_addextendedproperty @name = N'ModifiedDate', @value = N'', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ProjectType';
 
-
+EXEC sys.sp_addextendedproperty
+    @name = N'CreatedBy'
+    , @value = N'Carlos'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'dbo'
+    , @level1type = N'TABLE'
+    , @level1name = N'ProjectType';
 GO
-EXECUTE sp_addextendedproperty @name = N'SQLVERSION', @value = N'SQL Azure 12.0.2000.8', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ProjectType';
 
-
+EXEC sys.sp_addextendedproperty
+    @name = N'ModifiedDate'
+    , @value = N''
+    , @level0type = N'SCHEMA'
+    , @level0name = N'dbo'
+    , @level1type = N'TABLE'
+    , @level1name = N'ProjectType';
 GO
-EXECUTE sp_addextendedproperty @name = N'Version', @value = N'1.0', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ProjectType';
 
-
+EXEC sys.sp_addextendedproperty
+    @name = N'ModifiedBy'
+    , @value = N''
+    , @level0type = N'SCHEMA'
+    , @level0name = N'dbo'
+    , @level1type = N'TABLE'
+    , @level1name = N'ProjectType';
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'PK, IDENTITY', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ProjectType', @level2type = N'COLUMN', @level2name = N'ProjectTypeId';
 
-
+-- ******************************
+-- FIELD COMMENTS
+-- ******************************
+EXEC sys.sp_addextendedproperty
+    @name = N'MS_Description'
+    , @value = N'PK, IDENTITY'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'dbo'
+    , @level1type = N'TABLE'
+    , @level1name = N'ProjectType'
+    , @level2type = N'COLUMN'
+    , @level2name = N'ProjectTypeId';
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'The type of musical project', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ProjectType', @level2type = N'COLUMN', @level2name = N'ProjectTypeName';
 
-
+EXEC sys.sp_addextendedproperty
+    @name = N'MS_Description'
+    , @value = N'The type of musical project'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'dbo'
+    , @level1type = N'TABLE'
+    , @level1name = N'ProjectType'
+    , @level2type = N'COLUMN'
+    , @level2name = N'ProjectTypeName';
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'When the record was created in UTC', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ProjectType', @level2type = N'COLUMN', @level2name = N'CreatedUtc';
 
-
+EXEC sys.sp_addextendedproperty
+    @name = N'MS_Description'
+    , @value = N'When the record was created in UTC'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'dbo'
+    , @level1type = N'TABLE'
+    , @level1name = N'ProjectType'
+    , @level2type = N'COLUMN'
+    , @level2name = N'CreatedUtc'
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Who created the record', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ProjectType', @level2type = N'COLUMN', @level2name = N'CreatedBy';
 
-
+EXEC sys.sp_addextendedproperty
+    @name = N'MS_Description'
+    , @value = N'Who created the record'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'dbo'
+    , @level1type = N'TABLE'
+    , @level1name = N'ProjectType'
+    , @level2type = N'COLUMN'
+    , @level2name = N'CreatedBy'
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'When the record was modified in UTC', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ProjectType', @level2type = N'COLUMN', @level2name = N'ModifiedUtc';
 
-
+EXEC sys.sp_addextendedproperty
+    @name = N'MS_Description'
+    , @value = N'When the record was modified in UTC'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'dbo'
+    , @level1type = N'TABLE'
+    , @level1name = N'ProjectType'
+    , @level2type = N'COLUMN'
+    , @level2name = N'ModifiedUtc'
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Who modified the record', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ProjectType', @level2type = N'COLUMN', @level2name = N'ModifiedBY';
 
+EXEC sys.sp_addextendedproperty
+    @name = N'MS_Description'
+    , @value = N'Who modified the record'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'dbo'
+    , @level1type = N'TABLE'
+    , @level1name = N'ProjectType'
+    , @level2type = N'COLUMN'
+    , @level2name = N'ModifiedBy'
+GO
