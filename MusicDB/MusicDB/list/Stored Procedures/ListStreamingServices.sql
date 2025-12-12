1ee6e9ee006aed90310751b70df1e359b9511d6b
solrac031504 <carlos.gonzalez031504@gmail.com>
2025-12-11 20:36:35 -0500

    Created list schema and moved listing proc there

--- MusicDB/MusicDB/list/Stored Procedures/ListStreamingServices.sql
+++ MusicDB/MusicDB/list/Stored Procedures/ListStreamingServices.sql
@@ -2,13 +2,13 @@
 -- ======================================================
 -- Author:        Carlos Gonzalez
 -- Date Created:  2025-11-23
--- Description:   Lists all streaming services for parameter selection.
+-- Description:   Lists all streaming services
 ---------------------------------------------------------
 -- YYYY-MM-DD - Author - Change
 --
 -- ======================================================
 */
-CREATE PROCEDURE [reporting].[ListStreamingServices]
+CREATE PROCEDURE [list].[ListStreamingServices]
 
 AS
 
@@ -40,7 +40,7 @@ EXEC sys.sp_addextendedproperty
 	@name = N'Description'
 	, @value = N'Lists all streaming services for parameter selection'
 	, @level0type = N'SCHEMA'
-	, @level0name = N'reporting'
+	, @level0name = N'list'
 	, @level1type = N'PROCEDURE'
 	, @level1name = N'ListStreamingServices';
 GO
