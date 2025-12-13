abc681f3e0ab64e50b69c04b10ceda35df081f13
solrac031504 <carlos.gonzalez031504@gmail.com>
2025-12-13 14:44:49 -0500

    Added objects for user logins

---
+++ MusicDB/MusicDB/user/Stored Procedures/LoginUser.sql
@@ -0,0 +1,64 @@
+﻿/*
+-- ======================================================
+-- Author:        Carlos Gonzalez
+-- Date Created:  2025-12-13
+-- Description:   Login a user and update the count, time, and origin of the last login
+---------------------------------------------------------
+-- YYYY-MM-DD - Author - Change
+--
+-- ======================================================
+*/
+CREATE PROCEDURE [user].[LoginUser]
+	@pUserName				NVARCHAR(50)
+	, @pPassword			BINARY(32)
+	, @pLastLoginOrigin		NVARCHAR(50)
+	, @poAuthenticated		BIT OUTPUT
+AS
+
+BEGIN
+
+BEGIN TRY
+SET NOCOUNT ON
+
+/*
+-- ======================================================
+-- If login exists in the table, update the record
+-- ======================================================
+*/
+IF EXISTS (SELECT 1 FROM [user].[Login] AS u WHERE u.UserName = @pUserName AND u.[Password] = @pPassword AND u.IsActive = 1)
+	BEGIN
+		SET @poAuthenticated = 1;
+
+		SET XACT_ABORT ON
+		BEGIN TRANSACTION
+
+		UPDATE
+			ul
+		SET
+			ul.LoginCount = ul.LoginCount + 1
+			, ul.LastLoginDate = GETUTCDATE()
+			, ul.LastLoginOrigin = @pLastLoginOrigin
+			, ul.ModifiedBy = SUSER_SNAME()
+			, ul.ModifiedUtc = GETUTCDATE()
+		FROM
+			[user].[Login] AS ul
+		;
+
+		COMMIT TRANSACTION
+	END
+ELSE
+	BEGIN
+		SET @poAuthenticated = 0;
+	END
+
+END TRY
+
+BEGIN CATCH
+
+	;THROW
+
+END CATCH
+
+END
+
+GO
---
+++ MusicDB/MusicDB/user/Tables/Login.sql
@@ -0,0 +1,212 @@
+﻿CREATE TABLE [user].[Login]
+(
+	LoginId					INT IDENTITY(1, 1)		NOT NULL
+	, UserName				NVARCHAR(50)			NOT NULL
+	, [Password]			BINARY(32)				NOT NULL
+	, LoginCount			INT						NOT NULL CONSTRAINT [DF_Login_LoginCount] DEFAULT (( 0 ))
+	, LastLoginDate			DATETIME					NULL
+	, LastLoginOrigin		NVARCHAR(50)				NULL
+    , IsActive              INT                     NOT NULL CONSTRAINT [DF_Login_IsActive] DEFAULT (( 0 ))
+	, CreatedBy				NVARCHAR(255)			NOT NULL CONSTRAINT [DF_Login_CreatedBy] DEFAULT (SUSER_SNAME())
+	, CreatedUtc			DATETIME				NOT NULL CONSTRAINT [DF_Login_CreatedUtc] DEFAULT (GETUTCDATE())
+	, ModifiedBy			NVARCHAR(255)				NULL
+	, ModifiedUtc			DATETIME					NULL
+	, CONSTRAINT [PK_Login] PRIMARY KEY CLUSTERED (LoginId ASC)
+);
+GO
+
+-- ******************************
+-- INDEXES
+-- ******************************
+CREATE UNIQUE NONCLUSTERED INDEX [UX_Login_UserName]
+ON [user].[Login] (UserName)
+INCLUDE ([Password]);
+GO
+
+-- ******************************
+-- EXTENDED PROPERTIES
+-- ******************************
+EXEC sys.sp_addextendedproperty
+    @name = N'Description'
+    , @value = N'Data about individual artists'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login';
+GO
+
+EXEC sys.sp_addextendedproperty
+    @name = N'CODEOWNER'
+    , @value = N'CARLOS'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login';
+GO
+
+EXEC sys.sp_addextendedproperty
+    @name = N'SQLVERSION'
+    , @value = N'SQL Azure 12.0.2000.8'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login';
+GO
+
+EXEC sys.sp_addextendedproperty
+    @name = N'Version'
+    , @value = N'1.0'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login';
+GO
+
+EXEC sys.sp_addextendedproperty
+    @name = N'CreatedDate'
+    , @value = N'12/13/2025'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login';
+GO
+
+EXEC sys.sp_addextendedproperty
+    @name = N'CreatedBy'
+    , @value = N'Carlos'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login';
+GO
+
+EXEC sys.sp_addextendedproperty
+    @name = N'ModifiedDate'
+    , @value = N''
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login';
+GO
+
+EXEC sys.sp_addextendedproperty
+    @name = N'ModifiedBy'
+    , @value = N''
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login';
+GO
+
+-- ******************************
+-- FIELD COMMENTS
+-- ******************************
+EXEC sys.sp_addextendedproperty
+    @name = N'MS_Description'
+    , @value = N'PK, IDENTITY'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login'
+    , @level2type = N'COLUMN'
+    , @level2name = N'LoginId';
+GO
+
+EXEC sys.sp_addextendedproperty
+    @name = N'MS_Description'
+    , @value = N'The login username'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login'
+    , @level2type = N'COLUMN'
+    , @level2name = N'UserName';
+GO
+
+EXEC sys.sp_addextendedproperty
+    @name = N'MS_Description'
+    , @value = N'The hashed login password'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login'
+    , @level2type = N'COLUMN'
+    , @level2name = N'Password';
+GO
+
+EXEC sys.sp_addextendedproperty
+    @name = N'MS_Description'
+    , @value = N'How many times the login has been triggered'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login'
+    , @level2type = N'COLUMN'
+    , @level2name = N'LoginCount';
+GO
+
+EXEC sys.sp_addextendedproperty
+    @name = N'MS_Description'
+    , @value = N'The last time the login was matched in UTC'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login'
+    , @level2type = N'COLUMN'
+    , @level2name = N'LastLoginDate';
+GO
+
+EXEC sys.sp_addextendedproperty
+    @name = N'MS_Description'
+    , @value = N'Where the last login was triggered from'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login'
+    , @level2type = N'COLUMN'
+    , @level2name = N'LastLoginOrigin';
+GO
+
+EXEC sys.sp_addextendedproperty
+    @name = N'MS_Description'
+    , @value = N'Who created the record'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login'
+    , @level2type = N'COLUMN'
+    , @level2name = N'CreatedBy';
+GO
+
+EXEC sys.sp_addextendedproperty
+    @name = N'MS_Description'
+    , @value = N'When the record was created in UTC'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login'
+    , @level2type = N'COLUMN'
+    , @level2name = N'CreatedUtc';
+GO
+
+EXEC sys.sp_addextendedproperty
+    @name = N'MS_Description'
+    , @value = N'Who modified the record'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login'
+    , @level2type = N'COLUMN'
+    , @level2name = N'ModifiedBy';
+GO
+
+EXEC sys.sp_addextendedproperty
+    @name = N'MS_Description'
+    , @value = N'When the record was modified in UTC'
+    , @level0type = N'SCHEMA'
+    , @level0name = N'user'
+    , @level1type = N'TABLE'
+    , @level1name = N'Login'
+    , @level2type = N'COLUMN'
+    , @level2name = N'ModifiedUtc';
+GO
