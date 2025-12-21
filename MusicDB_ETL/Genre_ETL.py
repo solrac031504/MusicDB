76d64507302385f2d07b185af719aced491e2371
solrac031504 <carlos.gonzalez031504@gmail.com>
2025-12-20 19:45:50 -0500

    Loading JSON file. BRB going to eat

--- MusicDB_ETL/Genre_ETL.py
+++ MusicDB_ETL/Genre_ETL.py
@@ -1,1 +1,12 @@
 # Loads the Genre and GenreHierarchy tables from a JSON file
+import json
+import os
+
+# Get file path
+jsonPath = os.path.join(os.getcwd(), "MusicDB_ETL", "Data", "Genres.json")
+
+# Load JSON from file
+with open(jsonPath, 'r') as file:
+    genres = json.load(file)
+
+print(genres)
